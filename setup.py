88ba9da2849c9608c03121dbee9b6dc50a0f43f2
Ali Roth <wat6sv@virginia.edu>
2022-07-11 10:54:26 -0400

    Files added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/aliroth/Exercise-14.git',
+      author='Ali Roth',
+      author_email='wat6sv@virginia.edu',
+      license='MIT',
+      packages=['demo'])
